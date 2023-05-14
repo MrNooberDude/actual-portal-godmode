b18c45138ceeef0233415fe5ebb97bab99d52f62
Ice <67441866+MrNooberDude@users.noreply.github.com>
2023-05-14 06:11:24 -0400

    Update ModuleExample.java

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -1,10 +1,45 @@
 package com.example.addon.modules;
 
-import com.example.addon.Addon;
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.c2s.play.TeleportConfirmC2SPacket;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.addon.Addon;
 
 public class ModuleExample extends Module {
-    public ModuleExample() {
-        super(Addon.CATEGORY, "example", "An example module in a custom category.");
+    private List<TeleportConfirmC2SPacket> packets;
+
+    // Constructor
+
+    public PortalGodMode() {
+        super(Categories.Misc, "Portal God Mode", "Makes you invincible after leaving a nether portal.");
+    }
+
+    // Overrides
+
+    @Override
+    public void onActivate() {
+        this.packets = new ArrayList<>();
+    }
+
+    @Override
+    public void onDeactivate() {
+        if (!this.packets.isEmpty()) {
+            mc.getNetworkHandler().sendPacket(this.packets.get(this.packets.size() - 1));
+        }
+    }
+
+    // Packet Event
+
+    @EventHandler
+    private void onPacketSend(PacketEvent.Send event) {
+        if (event.packet instanceof TeleportConfirmC2SPacket packet) {
+            this.packets.add(packet);
+            event.setCancelled(true);
+        }
     }
 }
